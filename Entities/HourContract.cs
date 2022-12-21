c65203091d3678cc96744f45c4927a8196342c23
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-21 12:42:13 -0300

    Enums

--- Entities/HourContract.cs
+++ Entities/HourContract.cs
@@ -8,5 +8,20 @@ namespace composicao01Resolvido.Entities
 {
     internal class HourContract
     {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
     }
 }
