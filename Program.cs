66e0d21c2f4040781972bf3c08d0d92bbcca6bc9
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-26 08:52:21 -0300

    Criado classe Worker com 3 atributos, 1 associação Department, 1 HourContract e 3 métodos.

--- Program.cs
+++ Program.cs
@@ -2,3 +2,8 @@
 do usuário um mês e mostrar qual foi o salário do funcionário nesse mês
 */
 
+System.Console.Write("Enter department's name: ");
+string department = Console.ReadLine();
+System.Console.WriteLine("Enter worker data:");
+System.Console.Write("Name: ");
+string name = Console.ReadLine();
